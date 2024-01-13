24025f650bfd6ad61aeaa06e411de803a782691a
Nicole Puente <nicolepuente@gmail.com>
2024-01-12 23:23:30 -0600

    1st Try

---
+++ Module1/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @author Nicole Puente - npuente
+ * CIS175 - Spring 2024
+ * Jan 12, 2024
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!!");
+	}
+
+}
